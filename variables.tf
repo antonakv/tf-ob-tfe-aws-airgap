761af91e87ed0c0f166413a20c006541d0bb84bb
Anton Akulov <antonakv@users.noreply.github.com>
2021-08-10 17:27:00 +0200

    Adding IAM policies

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "key_name" {}
+variable "region" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "cidr_vpc" {}
+variable "cidr_subnet1" {}
+variable "cidr_subnet2" {}
+variable "cidr_subnet3" {}
+variable "cidr_subnet4" {}
+variable "db_password" {
+  type      = string
+  sensitive = true
+}
