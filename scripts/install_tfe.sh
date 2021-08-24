f54eb3bd22c14f702f449eb007af5034f1d24329
Anton Akulov <antonakv@users.noreply.github.com>
2021-08-24 16:51:39 +0200

    Installer script

--- scripts/install_tfe.sh
+++ scripts/install_tfe.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-#IPADDR=$(hostname -I | awk '{print $1}')
-#sudo ./install.sh no-proxy private-address=$IPADDR public-address=$IPADDR
+IPADDR=$(hostname -I | awk '{print $1}')
+mkdir install
+cd install
+aws s3 cp s3://aakulov-aws4-tfe-airgap . --recursive
+tar -xf latest.tar.gz
+sudo ./scripts/install.sh no-proxy private-address=$IPADDR public-address=$IPADDR
