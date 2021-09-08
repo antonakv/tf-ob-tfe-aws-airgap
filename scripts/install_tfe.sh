3e3554e0c9e0a53a6fbe073a6feb5eec7f74786b
Anton Akulov <antonakv@users.noreply.github.com>
2021-09-08 17:11:51 +0200

    Fixed cloud init

--- scripts/install_tfe.sh
+++ scripts/install_tfe.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 IPADDR=$(hostname -I | awk '{print $1}')
-cd /home/ubuntu
-mkdir install
-cd install
+mkdir -p /home/ubuntu/install
+cd /home/ubuntu/install
 aws s3 cp s3://aakulov-aws4-tfe-airgap . --recursive
 tar -xf latest.tar.gz
 aws s3 cp s3://aakulov-aws4-tfe-airgap/Terraform\ Enterprise\ -\ 557.airgap tfe557.airgap
+sudo rm Terraform\ Enterprise\ -\ 557.airgap
 sudo rm -rf /usr/share/keyrings/docker-archive-keyring.gpg
 yes | sudo ./install.sh no-proxy private-address=$IPADDR public-address=$IPADDR
