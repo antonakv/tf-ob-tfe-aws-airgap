d58913984553ea0023c4fda594a29a11da000b91
Anton Akulov <antonakv@users.noreply.github.com>
2022-02-17 15:22:13 +0100

    Test1

--- pre-req/main.tf
+++ pre-req/main.tf
@@ -79,21 +79,21 @@ resource "aws_s3_bucket_public_access_block" "aakulov-aws4-iam-role-airgap" {
   ignore_public_acls = true
 }
 
-resource "aws_s3_bucket_object" "latest.tar.gz" {
+resource "aws_s3_bucket_object" "latesttargz" {
   bucket = aws_s3_bucket.aws4-airgap.id
   key = "latest.tar.gz"
   source = "upload/latest.tar.gz"
   etag = filemd5("upload/latest.tar.gz")
 }
 
-resource "aws_s3_bucket_object" "license.rli" {
+resource "aws_s3_bucket_object" "licenserli" {
   bucket = aws_s3_bucket.aws4-airgap.id
   key = "license.rli"
   source = "upload/license.rli"
   etag = filemd5("upload/license.rli")
 }
 
-resource "aws_s3_bucket_object" "tfe-557.airgap" {
+resource "aws_s3_bucket_object" "tfe557airgap" {
   bucket = aws_s3_bucket.aws4-airgap.id
   key = "tfe-557.airgap"
   source = "upload/tfe-557.airgap"
